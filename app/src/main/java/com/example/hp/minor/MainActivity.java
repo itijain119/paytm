b6a3dc9b7aabf3d43629c7e1edfdedb762c01333
itijain119 <30702535+itijain119@users.noreply.github.com>
2017-11-24 23:51:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/hp/minor/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.hp.minor;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button b;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b=(Button)findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://paytm.com/"));
+                startActivity(intent);
+            }
+        });
+    }
+}
